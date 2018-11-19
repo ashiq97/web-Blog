70ff10d3a2351da03c678356d27810221d517c2b
ashiq97 <shishir4097@gmail.com>
2018-11-19 15:14:05 +0600

    sample added

---
+++ web_blog/Controllers/sampleController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web_blog.Controllers
+{
+    public class sampleController : Controller
+    {
+        // GET: sample
+        public ActionResult Index()
+        {
+            // hello
+            return View();
+        }
+    }
+}
